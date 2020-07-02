2dd1ed2601f9d19a18e48331b1fe4ec89601c526
Elijah Zobenko <ilya@zobenko.ru>
2020-07-02 21:45:37 +0300

    Фикстура пользователя

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordEncoderInterface $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $rawPassword = '123123123';
 
+        $user = new User();
+        $user->setName('Administrator')
+            ->setEmail('ilya@zobenko.ru')
+            ->setPassword($this->encoder->encodePassword($user, $rawPassword))
+            ->setRoles(array('ROLE_ADMIN', 'ROLE_USER'));
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
